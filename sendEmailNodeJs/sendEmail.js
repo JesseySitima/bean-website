234f0b971a4ae0f2a9b00c675cfabab531543234
unknown <jaysitima@gmail.com>
2023-12-21 12:51:40 +0200

    added nodemailer

--- sendEmailNodeJs/sendEmail.js
+++ sendEmailNodeJs/sendEmail.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mysql from 'mysql2'; // Import mysql2 instead of mysql
 import cors from 'cors';
+import nodemailer from 'nodemailer';
 
 const app = express();
 const port = 3000;
@@ -22,6 +23,15 @@ db.connect((err) => {
   console.log('Connected to MySQL database');
 });
 
+// Nodemailer configuration
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: 'jaysitima@gmail.com', // Replace with your Gmail email address
+    pass: 'bowe ahya zvnb duqv', // Replace with your Gmail password
+  },
+});
+
 // Enable CORS for all routes
 app.use(cors());
 
@@ -40,6 +50,32 @@ app.post('/store-data', (req, res) => {
   const { name, email, phone, date, department, message } = req.body;
 
   console.log('Received Form Data:', { name, email, phone, date, department, message });
+
+  // Sending email using Nodemailer
+  const mailOptions = {
+    from: 'jaysitima@gmail.com', // Replace with your Gmail email address
+    to: 'jaysitima@gmail.com', // Replace with the recipient's email address
+    subject: 'New Appointment',
+    html: `
+      <h1>New Appointment Information</h1>
+      <p><strong>Name:</strong> ${name}</p>
+      <p><strong>Email:</strong> ${email}</p>
+      <p><strong>Phone:</strong> ${phone}</p>
+      <p><strong>Date:</strong> ${date}</p>
+      <p><strong>Department:</strong> ${department}</p>
+      <p><strong>Message:</strong> ${message}</p>
+    `,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+      // You may handle errors related to sending email here
+    } else {
+      console.log('Email sent:', info.response);
+      // You can log or handle successful email sending here
+    }
+  });
 
   // Insert form data into MySQL database using prepared statements
   const sql = 'INSERT INTO appointments (name, email, phone, appointment_date, department, message) VALUES (?, ?, ?, ?, ?, ?)';
